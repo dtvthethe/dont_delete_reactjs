90981123f55ec60ba8c06690b13bc61afcd772cb
Đàm Thế <damthe@ams-MacBook-Pro.local>
2021-01-16 10:03:50 +0700

    todo reducer

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import TodoListContainer from "../containers/TodoListContainer";
 
 function App() {
   return (
     <div className="App">
-      
+      <TodoListContainer></TodoListContainer>
     </div>
   );
 }
