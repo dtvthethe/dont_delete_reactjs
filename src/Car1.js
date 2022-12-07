8d34cb83d0c3c31a3c0e81cf77dd312ec2c3dee1
thedtv-1961 <dam.trieu.van.the@sun-asterisk.com>
2022-12-08 01:24:33 +0700

    basic

---
+++ src/Car1.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Car1 extends Component {
+    render() {
+        let a = 100;
+
+        return (
+            <div>
+                Car duoc xay dung tu class {a}
+            </div>
+        );
+    }
+}
+
+export default Car1;
